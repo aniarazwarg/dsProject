379be4765d9c484fede677de25ab2a63e3e59d16
Aniara Zwarg <112500673+aniarazwarg@users.noreply.github.com>
2023-05-17 20:45:30 -0300

    adição tela a
    adição tela A

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, ImageBackground } from 
+'react-native';
+import { TextInput } from 'react-native-web';
+
+//importando a tela, após ter configurado no arquivo TelaA
+import { TelaA } from './tela/TelaA';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TelaA/>
+
+
+<ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      
+<Text style={styles.texto}>2ds3</Text>
+      <TextInput style={styles.input}placeholder = 'Usuário'></TextInput>
+      <TextInput style={styles.input} placeholder = 'Senha'></TextInput>
+      <Button title='Login'></Button>
+
+    </ImageBackground>
+      
+      
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const image = {uri: 'https://reactjs.org/logo-og.png'};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
   },
+  texto: {
+    color: 'white',
+    fontSize: 32,
+    lineHeight: 54,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+    
+  },
+
+  input:{
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+    fontStyle:'italic',
+  },
+ 
 });
+
+
+  
+
+
+
+
---
+++ tela/TelaA.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View,Button } from 'react-native';
+//importei o botao
+
+//exportando a função navigation.
+export function TelaA({navigation}) {
+  return (
+    <View style={{flex:1, backgroundColor:'red'}}>
+        <Button
+        title='Ir para tela B'
+        onPress={()=>navigation.navigate('telaB')}
+
+        ></Button>
+
+        
+
+    </View>
+  );
+}
