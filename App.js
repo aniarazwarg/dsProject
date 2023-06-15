8cd64e129f4c913a4239a5b8c621bd99a851bd67
Aniara Zwarg <112500673+aniarazwarg@users.noreply.github.com>
2023-06-14 22:55:53 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,84 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, ImageBackground } from 
-'react-native';
-import { TextInput } from 'react-native-web';
-
-
+import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { TelaA } from './tela/TelaA';
-import { TelaB } from './tela/TelaB';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { TelaA } from './telas/TelaA';
+import { TelaB } from './telas/TelaB';
 
 
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
   return (
-
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='TelaA'>
-        <Stack.Screen name='TelaA' component={TelaA} />
-        <Stack.Screen name='TelaB' component={TelaB} />
+      <Stack.Navigator initialRouteName='TelaB'>
+        <Stack.Screen name='TelaA' component={TelaA}/>
+        <Stack.Screen name='TelaB' component={TelaB}/>
       </Stack.Navigator>
     </NavigationContainer>
-
-);
+  );
 }
-
-
-
-
-//     <View style={styles.container}>
-//       <TelaA/>
-
-
-// <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      
-// <Text style={styles.texto}>2ds3</Text>
-//       <TextInput style={styles.input}placeholder = 'Usuário'></TextInput>
-//       <TextInput style={styles.input} placeholder = 'Senha'></TextInput>
-//       <Button title='Login'></Button>
-
-//     </ImageBackground>
-      
-      
-
-//       <StatusBar style="auto" />
-//     </View>
-
-// const image = {uri: 'https://reactjs.org/logo-og.png'};
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   image: {
-//     flex: 1,
-//     justifyContent: 'center',
-//   },
-//   texto: {
-//     color: 'white',
-//     fontSize: 32,
-//     lineHeight: 54,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//     backgroundColor: '#000000c0',
-    
-//   },
-
-//   input:{
-//     color: 'white',
-//     textAlign: 'center',
-//     backgroundColor: '#000000c0',
-//     fontStyle:'italic',
-//   },
- 
-// });
-
-
-  
-
-
-
-
